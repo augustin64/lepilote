5f7d35643b6bb3259de8c89d92b5b0b63405e141
augustin64 <augustin.lucas64@gmail.com>
2021-04-09 10:18:16 +0200

    ajout des données concernant les lignes de bus

---
+++ rtm/rtm_api.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+class BusLign():
+    def __init__(self,name='',id='') :
+        if name == '' and id == '':
+            raise Exception('you need to specify at least one argument')
+        
+        with open('./data/lignes_de_bus.json', 'r') as f:
+            data=f.read()
+
+        lignes_de_bus = json.loads(data)
+
+        if name != '' :
+            self.name = name
+            if id == '' :
+                self.id = [ i for i in lignes_de_bus.keys() i lignes_de_bus[i] == name ][0]
+
+        elif id != '' :
+            self.id = id
+            if name == '' :
+                self.name = lignes_de_bus[id]
+
