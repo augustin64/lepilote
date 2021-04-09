61181bea2d813b74ca0bf1a69eb3a987e309f119
augustin64 <augustin.lucas64@gmail.com>
2021-04-09 22:37:17 +0200

    modified rtm/rtm_api.py

--- rtm/rtm_api.py
+++ rtm/rtm_api.py
@@ -8,53 +8,51 @@ null = None #simplify json responses
 false = False
 true = True
 
+class BusLine():
+    def __init__(self,name:str='',ID:str='',color='#000000') :
+        self.color = color
+
+        if name == '' and ID == '':
+            raise Exception('you need to specify at least one argument')
+        path = os.path.join(Path(__file__).parent, "data/lines.json")
+
+        with open(path, 'r') as f:
+            data=f.read()
+        lines = json.loads(data)
+
+        if name != '' :
+            self.name = name
+            if ID == '' :
+                self.ID = lines[self.name]["ID"]
+
+        elif ID != '' :
+            self.ID = ID
+            if name == '' :
+                self.name = [ i for i in lines.keys() if lines[i]["ID"] == self.ID ][0]
+
+        self.LNE = lines[self.name]['LNE']
+
+    def __repr__(self):
+        return(self.name)
+
+    def get_routes(self):
+        url = "https://api.rtm.fr/front/getRoutes/" + self.LNE
+        content = eval(requests.get(url).text)['data']
+        directions = []
+        for i in content.keys():
+            directions.append(Schedules.BusDirection(
+                parent=self,
+                name=content[i]['DirectionStations'],
+                ID=content[i]['DirectionRef'],
+                RefNETEX=content[i]['refNEtex']
+                ))
+        self.routes = directions
+        return(directions)
+
 class Schedules():
     def __init__(self):
         """Just a subclass"""
         None
-
-
-    class BusLign():
-        def __init__(self,name:str='',ID:str='',color='#000000') :
-            self.color = color
-
-            if name == '' and ID == '':
-                raise Exception('you need to specify at least one argument')
-            path = os.path.join(Path(__file__).parent, "data/lignes.json")
-
-            with open(path, 'r') as f:
-                data=f.read()
-            lignes = json.loads(data)
-
-            if name != '' :
-                self.name = name
-                if ID == '' :
-                    self.ID = lignes[self.name]["ID"]
-
-            elif ID != '' :
-                self.ID = ID
-                if name == '' :
-                    self.name = [ i for i in lignes.keys() if lignes[i]["ID"] == self.ID ][0]
-
-            self.LNE = lignes[self.name]['LNE']
-
-        def __repr__(self):
-            return(self.name)
-
-        def get_routes(self):
-            url = "https://api.rtm.fr/front/getRoutes/" + self.LNE
-            content = eval(requests.get(url).text)['data']
-            directions = []
-            for i in content.keys():
-                directions.append(Schedules.BusDirection(
-                    parent=self,
-                    name=content[i]['DirectionStations'],
-                    ID=content[i]['DirectionRef'],
-                    RefNETEX=content[i]['refNEtex']
-                    ))
-            self.routes = directions
-            return(directions)
-
 
     class BusDirection() :
         def __init__(self,parent=None,name:str='',ID:str='0',RefNETEX:str=''):
@@ -136,4 +134,13 @@ def get_alerts(period='Today',LNE=None):
 
 class Alert():
     def __init__(self,data):
-        None
+        self.ItemIdentifier = data['ItemIdentifier']
+        self.InfoMessageIdentifier = data['InfoMessageIdentifier']
+        self.InfoChannelRef = data['InfoChannelRef']
+        self.ValidUntilTime = data['ValidUntilTime']
+        self.title = data['MessageA']
+        self.MessageB = data['MessageB']
+        self.validite = data['MessageC']
+        self.html = data['MessageD']
+        self.type = data['type']
+        self.AffectedLine = [BusLine(name=i['NAME']) for i in data['AffectedLine']]
