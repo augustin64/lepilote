4c58e8478b052bfdf70c9433573392b1cf3e0f78
augustin64 <augustin.lucas64@gmail.com>
2021-04-09 17:06:52 +0200

    Added RefNETEX

--- rtm/rtm_api.py
+++ rtm/rtm_api.py
@@ -5,14 +5,13 @@ import json
 
 class BusLign():
     def __init__(self,name: str='',id: str='') :
+        
         if name == '' and id == '':
             raise Exception('you need to specify at least one argument')
-
         path = os.path.join(Path(__file__).parent, "data/lignes.json")
-        print(path)
+
         with open(path, 'r') as f:
             data=f.read()
-
         lignes = json.loads(data)
 
         if name != '' :
@@ -27,16 +26,31 @@ class BusLign():
 
         self.LNE = lignes[self.name]['LNE']
 
+    def __repr__(self):
+        return(self.name)
+
     def get_routes(self):
         url = "https://api.rtm.fr/front/getRoutes/" + self.LNE
         content = eval(requests.get(url).text)['data']
+        directions = []
+        for i in content.keys():
+            directions.append(BusDirection(
+                parent=self,
+                name=content[i]['DirectionStations'],
+                ID=content[i]['DirectionRef'],
+                RefNETEX=content[i]['refNEtex']
+                ))
+        self.routes = directions
+        return(directions)
+
 
 class BusDirection() :
-    def __init__(self,parent=None,name='',ID='0'):
+    def __init__(self,parent=None,name='',ID='0',RefNETEX=''):
         self.ID=ID
         self.name=name
         self.parent=parent
+        self.RefNETEX=RefNETEX
     
     def __repr__(self):
-        return({'ID':self.ID,'Name':self.name,'parent':self.parent})
+        return(json.dumps({'ID':self.ID,'Name':self.name,'parent':str(self.parent)},indent=4))
         
