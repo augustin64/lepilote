a88c5e667475eb85e51f910b230923c98b4cdee1
augustin64 <augustin.lucas64@gmail.com>
2021-04-09 12:17:05 +0200

    changed path options

--- rtm/rtm_api.py
+++ rtm/rtm_api.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import os
 import requests
 import json
 
@@ -5,8 +7,10 @@ class BusLign():
     def __init__(self,name='',id='') :
         if name == '' and id == '':
             raise Exception('you need to specify at least one argument')
-        
-        with open('data/lignes_de_bus.json', 'r') as f:
+
+        path = os.path.join(Path(__file__).parent, "data/lignes_de_bus.json")
+        print(path)
+        with open(path, 'r') as f:
             data=f.read()
 
         lignes_de_bus = json.loads(data)
@@ -14,12 +18,12 @@ class BusLign():
         if name != '' :
             self.name = name
             if id == '' :
-                self.id = [ i for i in lignes_de_bus.keys() if lignes_de_bus[str(i)] == name ][0]
+                self.id = lignes_de_bus[str(name)]
 
         elif id != '' :
             self.id = id
             if name == '' :
-                self.name = lignes_de_bus[str(id)]
+                self.name = [ i for i in lignes_de_bus.keys() if lignes_de_bus[str(i)] == str(id) ][0]
 
     def get_routes(self):
         None
